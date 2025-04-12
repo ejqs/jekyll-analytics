0a97e2914208ad2484cb7cf91a3e4a76182b6a40
ejqs <62688314+ejqs@users.noreply.github.com>
2025-04-12 11:30:23 +0800

    Refactor CounterDotDev to use tracking_script instead of user for analytics rendering

--- lib/analytics/CounterDotDev.rb
+++ lib/analytics/CounterDotDev.rb
@@ -1,17 +1,15 @@
 class CounterDotDev
     def initialize(config)
-        if config['user'].nil?
+        if config['tracking_script'].nil?
             raise ArgumentError, 'Please use your counter.dev "user"'
         end
-
-        @user = config['user']
-
+        @tracking_script = config['tracking_script']
     end
 
     def render()
         str = """
     <!-- Counter.dev Analytics -->
-    <script>if(!sessionStorage.getItem(\"_swa\")&&document.referrer.indexOf(location.protocol+\"//\"+location.host)!== 0){fetch(\"https://counter.dev/track?\"+new URLSearchParams({referrer:document.referrer,screen:screen.width+\"x\"+screen.height,user:\"#{@user}\",utcoffset:\"2\"}))};sessionStorage.setItem(\"_swa\",\"1\");</script>
+    #{@tracking_script}
     <!-- End Counter.dev Analytics  -->
 """
         return str
