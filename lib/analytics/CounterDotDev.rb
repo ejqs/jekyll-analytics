cc9b370c2e98a619352dc7f5241f9612be6cf006
ejqs <62688314+ejqs@users.noreply.github.com>
2025-04-12 16:15:51 +0800

    Update CounterDotDev to include UTC offset description and fix script source reference

--- lib/analytics/CounterDotDev.rb
+++ lib/analytics/CounterDotDev.rb
@@ -4,11 +4,11 @@ class CounterDotDev
             raise ArgumentError, 'Please use the js "src" found when you add a website in counter.dev.'
         end
         @source = config['source']
+
         if config['data_id'].nil?
             raise ArgumentError, 'Please use the "data-id" found in the script when you add a website in counter.dev.'
         end
         @data_id = config['data_id']
-
 
         if config['data_utc_offset'].nil?
             raise ArgumentError, 'Please add a utc offset for the timezone you would want the data to be in.'
@@ -19,7 +19,7 @@ class CounterDotDev
     def render()
         str = """
     <!-- Counter.dev Analytics -->
-    <script src=\"#{@tracking_script}\" data-id=\"#{@data_id}\" data-utcoffset=\"#{@data_utc_offset}\"></script>
+    <script src=\"#{@source}\" data-id=\"#{@data_id}\" data-utcoffset=\"#{@data_utc_offset}\"></script>
     <!-- End Counter.dev Analytics  -->
 """
         return str
