a20caf71a844c5d524d5122d2cadbf4e94c3e616
ejqs <62688314+ejqs@users.noreply.github.com>
2025-04-13 15:50:58 +0800

    Refactor CounterDotDev tests to remove user references and use source, data_id, and data_utc_offset for initialization and rendering

--- test/CounterDotDev.rb
+++ test/CounterDotDev.rb
@@ -3,28 +3,39 @@ require "test/unit"
 
 class TestCounterDotDev < Test::Unit::TestCase
     def test_init
-        assert_raise( ArgumentError ) { CounterDotDev.new() }
-        assert_instance_of(CounterDotDev, CounterDotDev.new( {"user" => "SomeUser"} ))
+        assert_raise(ArgumentError) { CounterDotDev.new({}) }
+        assert_instance_of(CounterDotDev, CounterDotDev.new({
+            "source" => "https://cdn.counter.dev/script.js",
+            "data_id" => "0006dd92-4e28-4a49-b37e-e76fd3419ffc",
+            "data_utc_offset" => "2"
+        }))
     end
 
     def test_default_tracking_string
-        counterDotDev = CounterDotDev.new( {"user" => "SomeUser"} )
+        counterDotDev = CounterDotDev.new({
+            "source" => "https://cdn.counter.dev/script.js",
+            "data_id" => "00000-000000",
+            "data_utc_offset" => "2"
+        })
         assert_equal(counterDotDev.render(), 
-'''
+"""
     <!-- Counter.dev Analytics -->
-    <script>if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"SomeUser",utcoffset:"2"}))};sessionStorage.setItem("_swa","1");</script>
+    <script src=\"https://cdn.counter.dev/script.js\" data-id=\"00000-000000\" data-utcoffset=\"2\"></script>
     <!-- End Counter.dev Analytics  -->
-''')
+""")
     end
 
     def test_other_tracking_string
-        counterDotDev = CounterDotDev.new( {"user" => "SomeOtherUser"} )
+        counterDotDev = CounterDotDev.new({
+            "source" => "https://cdn.counter.dev/script.js",
+            "data_id" => "1234abcd-5678-efgh-ijkl-9876mnopqrst",
+            "data_utc_offset" => "5"
+        })
         assert_equal(counterDotDev.render(), 
-'''
+"""
     <!-- Counter.dev Analytics -->
-    <script>if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"SomeOtherUser",utcoffset:"2"}))};sessionStorage.setItem("_swa","1");</script>
+    <script src=\"https://cdn.counter.dev/script.js\" data-id=\"1234abcd-5678-efgh-ijkl-9876mnopqrst\" data-utcoffset=\"5\"></script>
     <!-- End Counter.dev Analytics  -->
-''')
+""")
     end
-
 end
